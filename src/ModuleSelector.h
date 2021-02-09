494e2056ac6cfdef2fa15fa894127b54cd427a60
xenia marchenko <ksumar4enko@gmail.com>
2021-02-09 12:24:53 +0300

    Перенес логику выбора модуля в отдельный файл

---
+++ src/ModuleSelector.h
@@ -0,0 +1,16 @@
+//
+// Created by Ксюша on 09.02.2021.
+//
+
+#ifndef BTIMETABLE_MODULESELECTOR_H
+#define BTIMETABLE_MODULESELECTOR_H
+
+#include <iostream>
+
+class ModuleSelector {
+public:
+    void selectModule();
+};
+
+
+#endif //BTIMETABLE_MODULESELECTOR_H
