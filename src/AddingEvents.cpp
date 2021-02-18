80e8cef4203bdf235af0af5cd4316d264a10da2f
togrammer <toprogrammer@yandex.ru>
2021-02-18 19:12:59 +0300

    Добавил валидацию данных

--- src/AddingEvents.cpp
+++ src/AddingEvents.cpp
@@ -4,40 +4,79 @@
 
 #include "AddingEvents.h"
 
+
 void EventAdder::addEvent() {
 
 
-    while(1){
-        std::cout << "1 -- Добавить событие\n";
-        std::cout << "2 -- Выход\n";
+    while(true){
+        std::cout << "1 -- Add an event\n";
+        std::cout << "2 -- Exit\n";
 
         int input;
         std::cin >> input;
         if(input == 2)
             break;
-
-        std::cout << "Введите название услуги\n";
+        std::string debug;
+        getline(std::cin, debug);//нужно из-за того что в буфере остается пустая строка
+        std::cout << "Enter the name of the event" << std::endl;
         std::string event;
-        getline(std::cin, event);//TODO: вставить в струкруру данных, валидацию данных
-
-        std::cout << "Введите имя специалиста\n";
+        getline(std::cin, event);
+        std::cout << "Enter the name of the specialist in the format LastName FirstName\n";
         std::string specialist;
-        getline(std::cin, specialist);//TODO: вставить в струкруру данных
-
-        std::cout << "Введите дату начала услуги в формате dd.mm.yy\n";
+        while(true) {
+            std::regex inputPattern(R"(^\s{0,}[a-z,A-Z]{1,20}\s{1,}[a-z,A-Z]{1,20}\s{0,})");
+            getline(std::cin, specialist);
+            if(std::regex_match(specialist, inputPattern)){
+                break;
+            }
+            std::cout << "Invalid input format, repeat again\n";
+        }
+        std::cout << "Enter the start date of the event in the format dd.mm.yy\n";
         int day, month, year;
         char c;
-        std::cin >> day >> c >> month >> c >> year;//TODO: вставить в струкруру данных, валидацию данных
-
-        std::cout << "Введите время начала услуги в формате hr.mt\n";
+        while(true) {
+            std::regex inputPattern(R"(^\s{0,}[0-9]{2,2}[.][0-9]{2,2}[.][0-9]{2,2}\s{0,})");
+            std::string in;
+            getline(std::cin, in);
+            if(std::regex_match(in, inputPattern)){
+                std::stringstream inputStream1(in);
+                inputStream1 >> day >> c >> month >> c >> year;
+                if(day >= 0 && day <= 31 && month >= 1 && month <= 12)
+                    break;
+            }
+            std::cout << "Invalid input format, repeat again\n";
+        }
+        std::cout << "Enter the event start time in the format hr.mt\n";
         int beginHour, beginMinute;
-        std::cin >> beginHour >> c >> beginMinute;//TODO: вставить в струкруру данных, валидацию данных
-
-        std::cout << "Введите время окончания услуги в формате hr.mt\n";
+        while(true) {
+            std::regex inputPattern(R"(^\s{0,}[0-9]{2,2}[.][0-9]{2,2}\s{0,})");
+            std::string in;
+            getline(std::cin, in);
+            if(std::regex_match(in, inputPattern)){
+                std::stringstream inputStream1(in);
+                inputStream1 >> beginHour >> c >> beginMinute;
+                if(beginHour >= 0 && beginHour <= 23 && beginMinute >= 0 && beginMinute <= 59)
+                    break;
+            }
+            std::cout << "Invalid input format, repeat again\n";
+        }
+        std::cout << "Enter the end time of the event in the format hr.mt\n";
         int endHour, endMinute;
-        std::cin >> endHour >> c >> endMinute;//TODO: вставить в струкруру данных, валидацию данных
-
-//        std::cout << "Ваша услуга"
+        while(true) {
+            std::regex inputPattern(R"(^\s{0,}[0-9]{2,2}[.][0-9]{2,2}\s{0,})");
+            std::string in;
+            getline(std::cin, in);
+            if(std::regex_match(in, inputPattern)){
+                std::stringstream inputStream1(in);
+                inputStream1 >> endHour >> c >> endMinute;
+                if(endHour >= 0 && endHour <= 23 && endMinute >= 0 && endMinute <= 59)
+                    break;
+            }
+            std::cout << "Invalid input format, repeat again\n";
+        }//TODO: вставить в струкруру данных
+        std::cout << "Your event: " << event << '\n';
+        std::cout << "Specialist: " << specialist << '\n';
+        std::cout << "data: " << day << "." << month << '.' << year << '\n';
+        std::cout << "time: " << beginHour << '.' << beginMinute << '-' << endHour << '.' << endMinute << '\n';
     }
-
 }
--- src/AddingEvents.h
+++ src/AddingEvents.h
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <string>
+#include <regex>
 
 class EventAdder{
 public:
